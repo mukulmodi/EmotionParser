bc2c737f01c0f011cbb848bc8ef9a133f8577e27
Mukul Modi <mukul.modi@gmail.com>
2017-09-07 12:17:15 -0500

    Adding file to hold basic emotions Scanner to read the file, create HM based on text file

--- src/ParserMain.java
+++ src/ParserMain.java
@@ -1,8 +1,39 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class ParserMain
 {
 
+	HashMap<String,Integer> emotionCount = new HashMap<String,Integer>();
 	public static void main(String[] args)
 	{
-		
+		String testPhrase = "I am happy";
+		setMap();
+		readString(testPhrase);
+	}
+
+	private static void readString(String phrase)
+	{
+		String[] words = phrase.split(" ");
+
+	}
+	private static void setMap()
+	{
+		String fileName = "emotion.txt";
+		try
+		{
+			Scanner fileScan = new Scanner(new File(fileName));
+			while(fileScan.hasNext())
+			{
+
+			}
+		}
+		catch (Exception FileNotFoundException)
+		{
+			System.out.println("Could not find or open file");
+		}
 	}
 }
